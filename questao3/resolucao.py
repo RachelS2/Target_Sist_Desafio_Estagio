7c98349f9ae2b3524436aac24d966f671e9aa190
RachelS2 <rachelbs@id.uff.br>
2024-09-13 18:24:36 -0300

    perf: usando dataframe em vez de encontrar valores com for

--- questao3/resolucao.py
+++ questao3/resolucao.py
@@ -7,18 +7,15 @@ DADOS_PATH = Path(r"questao3\dados.json")
 with open(DADOS_PATH, 'r') as arquivo:
     dados = json.load(arquivo)
 
-soma = 0
-qtd_dias = 0
+df = pd.DataFrame(dados)
+media = df['valor'].mean()
 
-valores = []
-for item in dados:
-    soma += item['valor']
-    qtd_dias += 1
-    valores.append(item['valor'])
+valor_max = df['valor'].max()
+valor_min = df['valor'].min()
 
-media = soma/qtd_dias
-valor_max = max(valores)
-valor_min = min(valores)
-
-print(f"O maior valor do mês é: {valor_max}")
-print(f"O menor valor do mês é: {valor_min}")
+df_acima_media = df[df['valor'] > media]
+dias_acima_media = len(df_acima_media)
+print(f"\nMédia de faturamento mensal: R$ {media:.3f}\n")
+print(f"O valor de faturamento foi maior do que a média em {dias_acima_media} dias do mês.")
+print(f"O maior valor de faturamento do mês é: {valor_max}")
+print(f"O menor valor de faturamento do mês é: {valor_min}")
