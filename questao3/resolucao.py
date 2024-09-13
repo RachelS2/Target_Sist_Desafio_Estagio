d94aabdfb1ada76a06525e4831de16d89e3a6148
RachelS2 <rachelbs@id.uff.br>
2024-09-13 18:18:37 -0300

    feat: encontrando valor min e max

--- questao3/resolucao.py
+++ questao3/resolucao.py
@@ -8,6 +8,17 @@ with open(DADOS_PATH, 'r') as arquivo:
     dados = json.load(arquivo)
 
 soma = 0
+qtd_dias = 0
+
+valores = []
 for item in dados:
-    dia = item['dia']
     soma += item['valor']
+    qtd_dias += 1
+    valores.append(item['valor'])
+
+media = soma/qtd_dias
+valor_max = max(valores)
+valor_min = min(valores)
+
+print(f"O maior valor do mês é: {valor_max}")
+print(f"O menor valor do mês é: {valor_min}")
