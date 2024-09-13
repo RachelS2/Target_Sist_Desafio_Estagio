3841bad78f76ace3bea93a7e695c8c594bd471ac
RachelS2 <rachelbs@id.uff.br>
2024-09-13 18:07:43 -0300

    feat: percorrendo json

--- questao3/resolucao.py
+++ questao3/resolucao.py
@@ -2,8 +2,12 @@ import json
 from pathlib import Path
 import pandas as pd
 
-DADOS_PATH = Path(r".\Target_Sist_Desafio_Estagio\questao3\dados.json")
+DADOS_PATH = Path(r"questao3\dados.json")
 
 with open(DADOS_PATH, 'r') as arquivo:
     dados = json.load(arquivo)
 
+soma = 0
+for item in dados:
+    dia = item['dia']
+    soma += item['valor']
